1dde779b887102da5f5c574f19337ad1bae45f7b
Johnson Shogbaike <G00389668@atu.ie>
2023-10-23 12:39:33 +0100

    created new application for week 5 lab

---
+++ src/test/java/com/example/week5lab/Week5LabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week5lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5LabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
